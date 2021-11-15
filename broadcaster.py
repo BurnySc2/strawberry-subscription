c88133d1e3e746ad78fb2ac8819aafa981aadb3d
BurnySc2 <gamingburny@gmail.com>
2021-11-15 09:36:32 +0100

    Add broadcast with subscriptions

---
+++ broadcaster.py
@@ -0,0 +1,61 @@
+from contextlib import asynccontextmanager
+from dataclasses import dataclass, field
+from typing import Any, Dict, Set, Optional
+from asyncio import Queue
+
+from loguru import logger
+
+
+@dataclass
+class Event:
+    channel: str
+    message: Any
+
+
+class Unsubscribed(Exception):
+    pass
+
+
+@dataclass
+class Subscriber:
+    queue: "Queue[Optional[Event]]" = field(default_factory=Queue)
+
+    async def __aiter__(self):
+        try:
+            while True:
+                item = await self.get()
+                yield item
+        except Unsubscribed:
+            pass
+        logger.info("unsubscribed")
+
+    async def get(self) -> Event:
+        item = await self.queue.get()
+        if item is None:
+            raise Unsubscribed()
+        return item
+
+
+@dataclass
+class Broadcast:
+    subscribers: Dict[str, Set[Queue]] = field(default_factory=dict)
+    published: Queue = Queue()
+
+    @asynccontextmanager
+    async def subscribe(self, channel: str) -> Subscriber:
+        subscriber = Subscriber()
+        subscribers_set = self.subscribers.get(channel, set())
+        subscribers_set.add(subscriber.queue)
+        self.subscribers[channel] = subscribers_set
+        yield subscriber
+        # Remove subscriber on context close
+        self.subscribers[channel].remove(subscriber.queue)
+        if not self.subscribers[channel]:
+            del self.subscribers[channel]
+        await subscriber.queue.put(None)
+        logger.info("unsubscribed")
+
+    async def publish(self, channel: str, message: Any):
+        event = Event(channel=channel, message=message)
+        for subscriber_queue in self.subscribers.get(channel, set()):
+            await subscriber_queue.put(event)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio
 from typing import AsyncGenerator, Set
 
 import uvicorn
@@ -7,8 +6,10 @@ from loguru import logger
 import strawberry
 from strawberry.fastapi import GraphQLRouter
 
+from broadcaster import Broadcast
+
 active_users: Set[str] = set()
-queue_user_joined: 'asyncio.Queue[str]' = asyncio.Queue()
+broadcast = Broadcast()
 
 
 @strawberry.type
@@ -21,33 +22,24 @@ class Query:
 @strawberry.type
 class Mutation:
     @strawberry.mutation
-    def chat_join_room(self, username: str) -> bool:
+    async def chat_join_room(self, username: str) -> bool:
         if username in active_users:
             return False
         active_users.add(username)
-        queue_user_joined.put_nowait(username)
+        await broadcast.publish("chatroom", username)
         return True
 
 
 @strawberry.type
 class Subscription:
     @strawberry.subscription
-    async def hello(self) -> AsyncGenerator[str, None]:
-        count = 0
-        while 1:
-            yield f'Subscription message {count}'
-            count += 1
-            await asyncio.sleep(1)
-
-    @strawberry.subscription
     async def chat_user_joined(self) -> AsyncGenerator[str, None]:
-        while 1:
-            if queue_user_joined.empty():
-                await asyncio.sleep(0.01)
-            else:
-                new_user = queue_user_joined.get_nowait()
-                logger.info(f'new user: {new_user}')
-                yield new_user
+        async with broadcast.subscribe(channel="chatroom") as subscriber:
+            logger.info("subscribed")
+            async for event in subscriber:
+                logger.info(event)
+                yield event.message
+        logger.info("unsubscribed")
 
 
 schema = strawberry.Schema(Query, mutation=Mutation, subscription=Subscription)
